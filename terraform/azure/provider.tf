5efcdc337e827315276381c0a54355ecb9f2d0e7
Ben Larabie <benlarabie@gmail.com>
2021-09-28 14:07:37 -0400

    hard coding values in provider for vanilla terraform

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -9,9 +9,9 @@ provider "azurerm" {
 terraform {
 
   backend "azurerm" {
-    resource_group_name  = var.dps_tf_store_rg
-    storage_account_name = var.dps_storage_account_name
-    container_name       = var.luis_instance_name
+    resource_group_name  = "DPSTerraformStore"
+    storage_account_name = "esdbdmdecdtfstate"
+    container_name       = "oas-unlock-bot-bdm-dev"
     key                  = "terraform.tfstate"
   }  
   required_providers {
