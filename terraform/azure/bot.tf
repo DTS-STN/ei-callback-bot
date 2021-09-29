d68f270f3028eeb1ab60a3348d11de69c0b6b6c1
bing gao <bing@freshworks.io>
2021-09-29 08:31:34 -0700

    changge bot channels location to global"

--- terraform/azure/bot.tf
+++ terraform/azure/bot.tf
@@ -4,7 +4,7 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_bot_channels_registration" "oas_callback_bot" {
   name                = "oas_callback_bot"
-  location            = var.location
+  location            = "global"
   resource_group_name = var.resource_group_name
   sku                 = "F0"
   microsoft_app_id    = "${data.azurerm_client_config.current.client_id}"
