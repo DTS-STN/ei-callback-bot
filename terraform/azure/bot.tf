40189903d590f72f12481f9bd7e65457e769a0d2
bing gao <bing@freshworks.io>
2021-12-01 10:50:59 -0800

    add iam binding to bot resource

--- terraform/azure/bot.tf
+++ terraform/azure/bot.tf
@@ -1,6 +1,6 @@
 data "azurerm_client_config" "current" {}
 
-
+data "azurerm_subscription" "primary" {}
 
 resource "azurerm_bot_channels_registration" "oas_callback_bot" {
   name                = "oas_callback_bot"
@@ -8,4 +8,10 @@ resource "azurerm_bot_channels_registration" "oas_callback_bot" {
   resource_group_name = var.resource_group_name
   sku                 = "F0"
   microsoft_app_id    = "${data.azurerm_client_config.current.client_id}"
+}
+
+resource "azurerm_role_assignment" "oas-callback-bot" {
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Contributor"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
